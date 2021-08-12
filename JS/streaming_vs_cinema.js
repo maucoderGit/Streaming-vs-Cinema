8a184b325b220ec07f02ab7f6266ffe5737e1a88
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-12 19:13:15 -0400

    create streaming platforms

---
+++ JS/streaming_vs_cinema.js
@@ -0,0 +1,23 @@
+function how_many_select(){
+    const input = document.getElementById("input")
+    return input.value
+}
+
+const platforms = [
+    id_1 ={
+    name: "Disney Plus", 
+    price: 8.99 
+    },
+    id_2 = {
+        name: "Netflix",
+        price: 7.99
+    },
+    id_3 = {
+        name: "Amazon Prime",
+        price: 12.99
+    },
+    id_4 = {
+        name: "HBO MAX",
+        price: 11.99
+    }
+]
