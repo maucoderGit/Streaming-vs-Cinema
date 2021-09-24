0e243af7338675a9b0c4b95cda7ffa753fad1a1f
maucoderGit <dreg5000.bbb@gmail.com>
2021-09-24 11:59:22 -0400

    correction in ids

--- src/main.js
+++ src/main.js
@@ -17,7 +17,8 @@ const platforms = [
     }
 ]
 
-const cinema_value = document.getElementById("price").value
+const cinema_value = document.getElementById("input_movies").value
+
 const how_many_select = () => {
     const input = document.getElementById("price")
     const input_value = parseInt(input.value)
@@ -43,5 +44,7 @@ function show_prices(){
     const cinema = how_many_select()
     const total_cinema = cinema_value * cinema
     
+    console.log(`el cine cuesta ${cinema_value}`)
+    console.log(`vas a ver ${cinema} peliculas`)
     console.log(stream_platforms_values())
 }
